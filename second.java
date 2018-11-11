4d3f512d4c1c00b0c7702513a6c7d1618d9a16ff
Dr neeraj shukla <neerajshukla28@gmail.com>
2018-11-11 19:40:58 +0530

    Update second.java

---
+++ second.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello second program" in the terminal window.
+      System.out.println("Hello second program");
+   }
+}
